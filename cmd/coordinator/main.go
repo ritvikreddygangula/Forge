60a8e5bb1d1497287a3fb77ed82a00e22b022fcc
ritvikreddygangula <ritvikreddygangula@gmail.com>
2026-09-19 14:29:38 -0700

    feat: wire coordinator and worker binaries for end-to-end job execution

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/ritvikreddygangula/forge/internal/coordinator"
+	"github.com/ritvikreddygangula/forge/internal/job"
+)
 
 func main() {
-	slog.Info("coordinator: not yet implemented")
+	addr := os.Getenv("COORDINATOR_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	store := job.NewMemoryStore()
+	srv := coordinator.NewServer(store)
+
+	slog.Info("coordinator starting", "addr", addr)
+	if err := http.ListenAndServe(addr, srv); err != nil {
+		slog.Error("coordinator exited", "error", err)
+		os.Exit(1)
+	}
 }
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ritvikreddygangula/forge/internal/worker"
+)
 
 func main() {
-	slog.Info("worker: not yet implemented")
+	coordinatorURL := os.Getenv("COORDINATOR_URL")
+	if coordinatorURL == "" {
+		coordinatorURL = "http://localhost:8080"
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	l := worker.NewLoop(coordinatorURL)
+	slog.Info("worker starting", "coordinator", coordinatorURL)
+	l.Run(ctx)
+	slog.Info("worker stopped")
 }
